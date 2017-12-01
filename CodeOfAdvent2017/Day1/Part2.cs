7f5689ec81defaa742619ffad692a0615d0fd6c7
Jonas Mellberg <jonas.mellberg@gmail.com>
2017-12-02 00:21:25 +0100

    Solution to Day1 Part 2

---
+++ CodeOfAdvent2017/Day1/Part2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeOfAdvent2017.Day1
+{
+    class Part2
+    {
+        static void Main()
+        {
+            var input = File.ReadAllText("Day1\\Input\\input.txt");
+
+            int sum = 0;
+            int step = input.Length / 2;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int value;
+                Int32.TryParse(input[i].ToString(), out value);
+
+                if (i + step >= input.Length)
+                {
+                    int diff = i + step - input.Length;
+                    sum += (input[i] == input[diff]) ? value : 0;
+                }
+                else
+                    sum += (input[i] == input[i + step]) ? value : 0;
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
