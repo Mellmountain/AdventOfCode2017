95086b4fd905286d5a1237217aea99c550d28653
Jonas Mellberg <jonas.mellberg@gmail.com>
2017-12-01 23:54:29 +0100

    Testing upload to git

---
+++ CodeOfAdvent2017/Day1/Part1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeOfAdvent2017.Day1
+{
+    class Part1
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
