6f00bf4d443c4dff8e3c7bf0b1743f4f6f1dabc6
Jonas Mellberg <jonas.mellberg@gmail.com>
2017-12-02 16:42:28 +0100

    Solution to day 2

---
+++ CodeOfAdvent2017/Day2/Part1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeOfAdvent2017.Day2
+{
+    class Part1
+    {
+        static void Main()
+        {
+            int checksum = 0;
+            var rows = File.ReadAllLines("Day2\\Input\\input.txt");
+            foreach(string row in rows)
+            {
+                string[] cols = row.Split('\t');
+                int lowest = Int32.MaxValue;
+                int highest = Int32.MinValue;
+                for (int i = 0; i < cols.Length; i++)
+                {
+                    int value;
+                    Int32.TryParse(cols[i], out value);
+
+                    lowest = value < lowest ? value : lowest;
+                    highest = value > highest ? value : highest;
+                }
+                checksum += highest - lowest;
+            }
+
+            Console.WriteLine(checksum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CodeOfAdvent2017/Day2/Part2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeOfAdvent2017.Day2
+{
+    class Part2
+    {
+        static void Main()
+        {
+            int sum = 0;
+            var rows = File.ReadAllLines("Day2\\Input\\input.txt");
+            foreach (string row in rows)
+            {
+                string[] cols = row.Split('\t');
+                for (int i = 0; i < cols.Length; i++)
+                {
+                    int value1;
+                    Int32.TryParse(cols[i], out value1);
+
+                    for(int j = 0; j < cols.Length; j++)
+                    {
+                        if (i == j)
+                            continue;
+                        int value2;
+                        Int32.TryParse(cols[j], out value2);
+
+                        if(value1 % value2 == 0)
+                        {
+                            sum += value1 >= value2 ? value1 / value2 : value2 / value1;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
