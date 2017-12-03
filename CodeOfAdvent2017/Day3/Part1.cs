96dda9e5a203805f809c34e12c985d7aac441ef0
Jonas Mellberg <jonas.mellberg@gmail.com>
2017-12-03 23:26:58 +0100

    Solution to Day3

---
+++ CodeOfAdvent2017/Day3/Part1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeOfAdvent2017.Day3
+{
+    class Part1
+    {
+        static void Main()
+        {
+            int targetValue = 265149;
+            Tuple<int, int> targetLocation;
+            int gridbase = 1;
+
+            while(gridbase*gridbase < targetValue)
+                gridbase += 2;
+
+            int portLocation = (gridbase - 1) / 2;
+
+            targetLocation = GetTargetLocation(targetValue, gridbase);
+            Console.WriteLine(String.Format("Manhattan distance = {0}", Math.Abs(targetLocation.Item1 - portLocation) + Math.Abs(targetLocation.Item2 - portLocation)));
+            Console.ReadLine();
+        }
+
+        public static Tuple<int,int> GetTargetLocation(int target, int gridbase)
+        {
+            /// Side = 1 => Bottom side of square
+            /// Side = 2 => Left side of square
+            /// Side = 3 => Top side of square
+            /// Side = 4 => Right side of square
+            int targetx;
+            int targety;
+            for (int side = 1; side <= 4; side++)
+            {
+                if(target >= (gridbase*gridbase) - (gridbase-1) * side)
+                {
+                    int min = (gridbase*gridbase) - (gridbase - 1) * side;
+                    int max = min + (gridbase - 1);
+                    if(side % 2 == 0)
+                    {
+                        targetx = side == 2 ? 0 : gridbase - 1;
+                        targety = min == target ? 0 : max - target;
+                    }
+                    else
+                    {
+                        /* special case for top-side (index reversed) */
+
+                        targetx = side == 1 ? Math.Abs(min - target) : max - target;
+                        targety = side == 1 ? gridbase - 1 : 0;
+                    }
+                    return new Tuple<int, int>(targetx, targety);
+                }
+            }
+            return null;
+        }
+    }
+}
