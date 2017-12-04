6273dfe9460047a1ee23847c7df24e903a07092e
Jonas Mellberg <jonas.mellberg@gmail.com>
2017-12-04 23:03:10 +0100

    Solution to Day 4

---
+++ CodeOfAdvent2017/Day4/Part1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeOfAdvent2017.Day4
+{
+    class Part1
+    {
+        static void Main()
+        {
+            int validPhrases = 0;
+            var passphrases = File.ReadAllLines("Day4\\Input\\input.txt");
+            foreach (string passphrase in passphrases)
+            {
+                if (ValidatePassphrase(passphrase))
+                    validPhrases++;
+            }
+
+            Console.WriteLine("Valid passphrases: " + validPhrases);
+            Console.ReadLine();
+        }
+
+        static bool ValidatePassphrase(string passphrase)
+        {
+            string[] words = passphrase.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (i == j) /* Don't check ourself */
+                        continue;
+                    if (words[i] == words[j])
+                        return false;
+                }
+            return true;
+        }
+    }
+}
+
