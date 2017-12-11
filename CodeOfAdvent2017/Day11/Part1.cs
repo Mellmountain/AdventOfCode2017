58a12ef75ee001ede172ed1eec519cbeac5d1154
Jonas Mellberg <jonas.mellberg@gmail.com>
2017-12-11 20:27:30 +0100

    Solution to Day 11

---
+++ CodeOfAdvent2017/Day11/Part1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AdventOfCode2017.Day11
+{
+    class Part1
+    {
+        [STAThread]
+        static void Main()
+        {
+            string input = File.ReadAllText("Day11\\Input\\input.txt");
+            string[] directions = input.Split(',');
+
+            int x = 0;
+            int y = 0;
+            foreach(string direction in directions)
+            {
+                switch(direction)
+                {
+                    case "s":
+                            y++;
+                            break;
+                    case "se":
+                            x++;
+                            break;
+                    case "sw":
+                            x--;
+                            y++;
+                            break;
+                    case "n":
+                            y--;
+                            break;
+                    case "ne":
+                            x++;
+                            y--;
+                            break;
+                    case "nw":
+                            x--;
+                            break;
+                    default:
+                            Console.WriteLine("Unknown direction!");
+                            break;
+                }
+            }
+            int distance = (Math.Abs(0 - x) + Math.Abs(0 - x - y) + Math.Abs(0 - y)) / 2;
+            Console.WriteLine(distance);
+            Clipboard.SetText(distance.ToString());
+            Console.ReadLine();
+        }
+    }
+}
