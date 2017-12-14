f5b54a1f028ad5630ffc943a557c4b4dbc53e9ee
Jonas Mellberg <jonas.mellberg@gmail.com>
2017-12-14 07:38:34 +0100

    Solutio to Day13 (Only Part1)

---
+++ CodeOfAdvent2017/Day13/Part2_slow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017.Day13
+{
+    class Part2_slow
+    {
+        static void Main()
+        {
+            string[] input = File.ReadAllLines("Day13\\Input\\input.txt");
+            int delay = 0;
+            while (true)
+            {                
+                List<Layer> firewall = Part1.SetupFirewallLayers(input);
+                SetupFireWall(firewall, delay);
+
+                if (SendPacketThroughFirewall(firewall))
+                    break;
+                delay++;
+            }
+            Console.WriteLine("Delay: " + delay);
+            Console.ReadLine();
+        }
+        
+        /// <summary>
+        /// The time it takes to set up FW gets bad when delay gets large
+        /// </summary>        
+        private static void SetupFireWall(List<Layer> firewall, int delay)
+        {
+            while(delay != 0)
+            {
+                foreach(Layer layer in firewall)
+                {
+                    layer.MoveScanner();
+                }
+                delay--;
+            }
+        }
+
+        private static bool SendPacketThroughFirewall(List<Layer> firewall)
+        {
+            for (int packetDepth = 0; packetDepth <= firewall.Last().depth; packetDepth++)
+            {
+                foreach (Layer layer in firewall)
+                {
+                    if (layer.ThreatDetected(packetDepth))
+                        return false;
+                    layer.MoveScanner();
+                }
+            }
+
+            return true;
+        }
+    }
+}
