f0b95f3568607286de6cafda833602440ac29bc9
Jonas Mellberg <jonas.mellberg@gmail.com>
2017-12-14 22:41:48 +0100

    Solution to Day 14

---
+++ CodeOfAdvent2017/Day14/Part1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017.Day14
+{
+    class Part1
+    {
+        static void Main()
+        {
+            string input = "flqrgnkx";
+            int[,] memory = new int[128, 128];
+
+            /* generate the hashes */
+            FillDisk(input, memory, 1);
+
+            int usedMemory = 0;
+            for (int i = 0; i < 128; i++)
+                for (int j = 0; j < 128; j++)
+                {
+                    if (memory[i, j] == 1)
+                        usedMemory++;
+                }
+
+            Console.WriteLine("Used memory:" + usedMemory);
+            Console.ReadLine();
+        }
+
+        public static void FillDisk(string input, int[,] memory, int fillValue)
+        {
+            for (int i = 0; i < 128; i++)
+            {
+                string hexhash = Day10.Part2.KnotHash(input + "-" + i);
+                char[] hexchars = hexhash.ToCharArray();
+                string binaryRepresentation = ConverToBinary(hexchars);
+
+                for (int k = 0; k < 128; k++)
+                {
+                    if (binaryRepresentation[k] == '0')
+                        memory[i, k] = 0;
+                    else
+                        memory[i, k] = fillValue;
+                }
+            }
+        }
+
+        private static string ConverToBinary(char[] hexchars)
+        {
+            string output = "";
+            for (int j = 0; j < hexchars.Length; j++)
+            {
+                string binary = Convert.ToString(Convert.ToInt32(hexchars[j].ToString(), 16), 2);
+                while (binary.Length != 4)
+                    binary = binary.Insert(0, "0");
+                output += binary;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ CodeOfAdvent2017/Day14/Part2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017.Day14
+{
+    class Part2
+    {
+        static void Main()
+        {
+            string input = "xlqgujun";
+            int[,] memory = new int[128, 128];
+            Part1.FillDisk(input, memory, int.MaxValue);
+            int groupCount = 1;
+            for(int y = 0; y < 128; y++)
+                for(int x = 0; x < 128; x++)
+                {
+                    FloodFill(new Tuple<int, int>(x, y), groupCount, ref memory);
+                    if (memory[y, x] == groupCount)
+                        groupCount++;
+                }
+
+            Console.WriteLine("Group count: " + (groupCount - 1));
+            Console.ReadLine();
+        }
+
+        private static void FloodFill(Tuple<int, int> from, int groupValue, ref int[,] memory)
+        {
+            if (memory[from.Item2, from.Item1] == 0)
+                return;
+            if (memory[from.Item2, from.Item1] <= groupValue)
+                return;
+            if (memory[from.Item2, from.Item1] == int.MaxValue)
+                memory[from.Item2, from.Item1] = groupValue;
+
+            if (from.Item2 <= 126)
+                FloodFill(new Tuple<int, int>(from.Item1, from.Item2 + 1), groupValue, ref memory); /* south */
+            if(from.Item2 > 0)
+                FloodFill(new Tuple<int, int>(from.Item1, from.Item2 - 1), groupValue, ref memory); /* north */
+            if(from.Item1 > 0)
+                FloodFill(new Tuple<int, int>(from.Item1 - 1, from.Item2), groupValue, ref memory); /* west */
+            if (from.Item1 <= 126)
+                FloodFill(new Tuple<int, int>(from.Item1 + 1, from.Item2), groupValue, ref memory); /* east */
+            return;
+        }
+    }
+}
